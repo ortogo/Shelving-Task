63cb29a021096d1579adbbd45fe8e5e6b32bd9b3
Vladislav Volkov <ortogorbd@gmail.com>
2019-03-12 15:51:05 +0200

    Added traversa test

---
+++ StillageTaskTest/TraversaCutTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ortogo.SolidWorks.StillageTask;
+
+namespace StillageTaskTest
+{
+    [TestClass]
+    public class TraversaCutTests
+    {
+        [TestMethod]
+        public void TestSelect()
+        {
+            TestSets.InitFirst();
+            var traversa = new TraversaCut().Select();
+
+            Assert.AreNotEqual(null, traversa, "Traversa is not found");
+            Assert.AreEqual("ТК.1050.40.40.15", traversa.ToString(), "Traversa is not correct");
+
+            TestSets.InitSecond();
+            traversa = new TraversaCut().Select();
+
+            Assert.AreNotEqual(null, traversa, "Traversa is not found");
+            Assert.AreEqual("ТК.1800.40.40.15", traversa.ToString(), "Traversa is not correct");
+
+            TestSets.InitThird();
+            traversa = new TraversaCut().Select();
+
+            Assert.AreNotEqual(null, traversa, "Traversa is not found");
+            Assert.AreEqual("ТК.2700.40.40.15", traversa.ToString(), "Traversa is not correct");
+        }
+    }
+}
