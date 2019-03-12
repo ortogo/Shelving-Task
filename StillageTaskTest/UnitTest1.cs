a9ef768f5fc54baa5f3cd7c30c4e62bc5a9ec8ef
Vladislav Volkov <ortogorbd@gmail.com>
2019-03-12 14:55:14 +0200

    Created test project

---
+++ StillageTaskTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StillageTaskTest
+{
+    [TestClass]
+    public class StillageTaskTests
+    {
+        [TestMethod]
+        public void TestStillageCalculation()
+        {
+        }
+    }
+}
