7603a71ea57639a6670a4d50010bc878c93a63d6
Vladislav Volkov <ortogorbd@gmail.com>
2019-03-12 13:54:16 +0200

    fixed assembly info

--- StillageTask/Properties/AssemblyInfo.cs
+++ StillageTask/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AngelSix.SolidWorks.BlankAddin")]
+[assembly: AssemblyTitle("StillageTask")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AngelSix.SolidWorks.BlankAddin")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("StillageTask")]
+[assembly: AssemblyCopyright("Copyright © Ortogo 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
